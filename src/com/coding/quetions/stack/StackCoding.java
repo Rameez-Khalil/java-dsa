1b7b8488bcead4fe896adf47454128e26b0f096d
rameez <rameezkhalil23@gmail.com>
2024-01-13 09:17:16 -0800

    stacks coding questions

--- src/com/coding/quetions/stack/StackCoding.java
+++ src/com/coding/quetions/stack/StackCoding.java
@@ -7,6 +7,11 @@ public class StackCoding {
         System.out.println(reverseString("Hello"));
         System.out.println(isValidParenthesis("()"));
         System.out.println(isValidParenthesis("()()["));
+        Stack<Integer> stack = new Stack<>();
+        stack.push(50);
+        stack.push(30);
+        stack.push(100);
+        System.out.println(sortStack(stack));
     }
 
     //Reverse a string.
@@ -67,6 +72,32 @@ public class StackCoding {
         }
 
         return stack.isEmpty();
+    }
+
+    //SORT A STACK.
+    /*
+        1. Create a temporary stack.
+        2. Read top element from the original stack, and store it in a variable.
+        3. Read the temporary stack, if its not empty and if its current element / top element is greater than the
+        one  which we are trying to add above it then we must:
+            Add back the temporary stack top to the original stack.
+            Store the current value which had store in the 2nd step to the temporary stack.
+        4. Repeat the above steps until the original stack is empty.
+        5. Return the temporary stack.
+     */
+
+    public static Stack<Integer> sortStack(Stack<Integer> stack){
+        Stack<Integer> temporaryStack = new Stack<Integer>();
+        while(!stack.isEmpty()){
+            int currentItem = stack.pop();
+            while(!temporaryStack.isEmpty() && temporaryStack.peek()>currentItem){
+                stack.push(temporaryStack.pop());
+            }
+
+            temporaryStack.push(currentItem);
+        }
+
+        return temporaryStack;
     }
 
 
