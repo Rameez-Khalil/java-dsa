731b42363dbafd541053ce2b2c4e8a5d19f48734
rameez <rameezkhalil23@gmail.com>
2024-02-03 05:20:25 -0800

    linked list questions

---
+++ src/com/coding/quetions/linkedlist/Questions.java
@@ -0,0 +1,33 @@
+package com.coding.quetions.linkedlist;
+
+public class Questions {
+
+
+    //MIDDLE NODE.
+//    public static void middleNode() {
+//        //Tortoise and Hare approach, slow and fast pointer technique.
+//        Node slow = head;
+//        Node fast = head;
+//        while (fast != null && fast.next != null) {
+//            slow = slow.next;
+//            fast = fast.next.next;
+//        }
+//
+//        return slow;
+//
+//    }
+
+    //HAS LOOP.
+//    public boolean hasLoop() {
+//        Node slow = head;
+//        Node fast = head;
+//        while (fast != null & fast.next != null) {
+//            slow = slow.next;
+//            fast = fast.next.next;
+//            if (slow == fast) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+}
